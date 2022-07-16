5341ac7dde56424c180fbec6c23476725dc1181c
marcojr73 <marcojuniorengineer@gmail.com>
2022-07-16 18:39:28 -0300

    crud cards

---
+++ src/repositories/cardsRepositories.ts
@@ -0,0 +1,57 @@
+import client from "../config/dataBase.js"
+
+import {cards} from "@prisma/client"
+
+export type cardsData = Omit<cards, "id" | "createdAt">
+
+
+async function getAllCardsByUserId(userId: number){
+    return await client.cards.findMany({
+        select: {
+            nameCard: true,
+            cvc: true,
+            expirationDate: true,
+            title: true,
+            password: true,
+            userId: true,
+            number: true,
+            isVirtual: true
+        },
+        where: {userId}
+    })
+}
+
+async function insertAnNewCardOnBank(dataCard: cardsData){
+    await client.cards.create({
+        data: dataCard
+    })
+}
+
+async function getCardById(id: number){
+    return await client.cards.findUnique({
+        select:{
+            nameCard: true,
+            cvc: true,
+            expirationDate: true,
+            title: true,
+            password: true,
+            userId: true,
+            number: true,
+            isVirtual: true
+        },
+        where: {id}
+    })
+}
+
+async function deleteCardById(id){
+    await client.cards.delete({
+        where:{id}
+    })
+}
+
+export {
+    getAllCardsByUserId,
+    insertAnNewCardOnBank,
+    getCardById,
+    deleteCardById
+}
--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -17,8 +17,20 @@ const dataNotesIsCorrect = joi.object({
     note: joi.string().max(1000).required()
 })
 
+const dataCardIsCorrect = joi.object({
+    number: joi.string().required(), 
+    nameCard: joi.string().required(), 
+    cvc: joi.string().required(), 
+    expirationDate: joi.string().required(), 
+    password: joi.string().required(), 
+    isVirtual: joi.boolean().required(), 
+    type: joi.string().required(),
+    title: joi.string().required()
+})
+
 export {
     dataSignInUp,
     dataCredentialsisCorrect,
-    dataNotesIsCorrect
+    dataNotesIsCorrect,
+    dataCardIsCorrect
 }
