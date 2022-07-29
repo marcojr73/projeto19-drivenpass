5fd882caec2ffe2ba8e7281f9563b1cb00130691
Vinicius <vinigugelmin@hotmail.com>
2022-07-29 16:38:43 -0300

    Preparando revisão de código

--- prisma/migrations/20220714194619_top/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `CreatedAt` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "CreatedAt",
-ALTER COLUMN "createdAt" SET DATA TYPE TEXT;
